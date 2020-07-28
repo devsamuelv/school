ee9bd78322bc3d5158a9405ce6bd6139f90da3f7
Samuel Villegas <52802289+DevSamuelV@users.noreply.github.com>
2020-07-27 22:19:53 -0400

    Update Assiment_2_06.py

--- Assiment_2_06.py
+++ Assiment_2_06.py
@@ -1,26 +1,49 @@
-items = {
-    {
-        "name": input("Enter the item name >> "),
-        "price": input("Enter the item price >> ")
-    },
+import math
+import json
 
-    {
-        "name": input("Enter the item name >> "),
-        "price": input("Enter the item price >> ")
-    }
-}
-
+itemNumber = 1 # input("Number of items >> ")
 tax = 6
-shipping = 5.99
+flat_shipping = 5
+
+items = [
+        
+]
+
+def printInfo(input):
+    print("[info]:: " + str(input))
+
+def countItems():
+    count = 0
+
+    for i in items:
+        count + 1
+
+    # to make sure its an int
+    return int(count)
+
+def insert(data):
+    item_count = countItems()
+
+    items.insert(item_count + 1, data)
+
+# create json data for each item
+
+for num in range(0, int(itemNumber)):
+    name = input("Enter the name of the item >> ")
+    price = input("Enter the price of the item >> ")
+
+    insert({
+        "name": name,
+        "price": price 
+    })
+
+    # printInfo(num)
 
 def calTax():
-    return tax / 100 
+    return tax / 100
 
 def calFullPrice(price: float):
-    return round(shipping + (float(price) + (calTax() * float(price))))
+    return (price + calTax()) + flat_shipping
 
-for item in items:
-    print(item.price)
-
-for item in items:
-    print(calFullPrice(float(item.price)))
+for i in items:
+    print("Item: " + str(i.get("name")) + " Will cost: " + "$" + str(calFullPrice(float(i.get("price")))) + " to ship with tax")
